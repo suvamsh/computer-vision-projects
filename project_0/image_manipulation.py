c998ae97b34c30e3804271e10b608f3a4815c39f
Suvamsh Shivaprasad <suvamsh@Suvamshs-MacBook-Air.local>
2014-08-29 20:20:42 -0500

    project complete

--- project_0/image_manipulation.py
+++ project_0/image_manipulation.py
@@ -9,3 +9,29 @@ unit tests pass.
 """
 
 # TODO: Implement!
+
+import cv2
+
+
+def flip_image(image, horizontal, vertical):
+    if horizontal and vertical:
+        return cv2.flip(image, -1)
+    elif horizontal and not vertical:
+        return cv2.flip(image, 1)
+    elif not horizontal and vertical:
+        return cv2.flip(image, 0)
+    return image
+
+
+def negate_image(nyc):
+    return ~nyc
+
+
+def swap_red_and_green(nyc):
+    image = nyc
+    for i in range(len(nyc)):
+        for j in range(len(nyc[i])):
+            image[i][j][0] = nyc[i][j][1]
+            image[i][j][1] = nyc[i][j][0]
+            image[i][j][2] = nyc[i][j][2]
+    return image
