5874456ba3b19f1b2cc2e13d1f3dabe078cb4888
Suvamsh Shivaprasad <suvamsh@wireless-10-147-38-204.public.utexas.edu>
2014-11-02 14:57:17 -0600

    added face tracking code and dataset

---
+++ project_3/face_track.py
@@ -0,0 +1,35 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture('test_data/face.mov')
+# Create the haar cascade
+faceCascade = cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
+count = 0
+while (cap.isOpened()):
+	count = count + 1
+	# take first frame of the video
+	ret,frame = cap.read()
+	#cv2.imshow("face",frame)
+	#gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	# Detect faces in the image
+	faces = faceCascade.detectMultiScale(frame,
+    									scaleFactor=1.3,
+    									minNeighbors=5,
+    									minSize=(30, 30),
+    									flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+										)
+	# Draw a rectangle around the faces
+	for (x, y, w, h) in faces:
+		#print x-w, y-h, x+w, y+h
+		cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+	# Display the resulting frame
+	cv2.imshow('Video', frame)
+	#print count
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
+print count
