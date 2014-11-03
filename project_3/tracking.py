757fd77ffb80af59c7de28d58e0961831ba64cfc
Nikhil Dixit <njdixit@gmail.com>
2014-11-03 16:23:53 -0600

    All balls tracking! GGNORE.

--- project_3/tracking.py
+++ project_3/tracking.py
@@ -8,17 +8,22 @@ import math
 import numpy as np
 
 
-def get_initial_position(frame):
+def get_initial_position(frame, hough_parameters):
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    #circles = cv2.HoughCircles(frame, cv2.cv.CV_HOUGH_GRADIENT, 7, 1000,       WORKS FOR 1, 2, 4
+     #                           param1=40, param2=20, minRadius=0, maxRadius=0)
+    
+    #circles = cv2.HoughCircles(frame, cv2.cv.CV_HOUGH_GRADIENT, 2, 1000,
+     #                           param1=40, param2=40, minRadius=0, maxRadius=0)
+    
+    circles = cv2.HoughCircles(frame, cv2.cv.CV_HOUGH_GRADIENT, **hough_parameters)
 
-    circles = cv2.HoughCircles(frame, cv2.cv.CV_HOUGH_GRADIENT, 2, 1000,
-                                param1=40, param2=20, minRadius=0, maxRadius=0)
     circles = np.uint16(np.around(circles))
-    #print circles
     return circles[0][0]
 
 
-def track_ball(video):
+def track_ball(video, hough_parameters, initial_radius_scalar = 1):
 
     # data structure to keep track of all frames
     frame_history = []
@@ -32,43 +37,44 @@ def track_ball(video):
     current_frame = 1
 
     # get initial location of ball
-    initial_circle = get_initial_position(frame)
+    initial_circle = get_initial_position(frame, hough_parameters)
 
     # setup return structure
     coords = []
 
     # setup initial location of window
-    r,h,c,w = initial_circle[0] - initial_circle[2], initial_circle[2] * 2, initial_circle[1] - initial_circle[2], initial_circle[2] * 2
+    r,h,c,w = int(initial_circle[0] - initial_circle[2]), int(initial_circle[2] * 2 * initial_radius_scalar), initial_circle[1] - initial_circle[2], int(initial_circle[2] * 2 * initial_radius_scalar)
     track_window = (c,r,w,h)
     coords.append((initial_circle[0] - initial_circle[2], initial_circle[1] - initial_circle[2], initial_circle[0] + initial_circle[2], initial_circle[1] + initial_circle[2]))
-    #print coords
 
     # set up the ROI for tracking
+    
     roi = frame[r:r+h, c:c+w]
     hsv_roi =  cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv_roi, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
     roi_hist = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180])
     cv2.normalize(roi_hist,roi_hist,0,255,cv2.NORM_MINMAX)
+    
 
     # Setup the termination criteria, either 20 iterations or move by atleast 1 pt
-    term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 20, 1)
+    term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 30, 1)
 
     
     while(1):
         ret ,frame = video.read()
 
         if ret == True:
-
             hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
             dst = cv2.calcBackProject([hsv],[0],roi_hist,[0,180],1)
+
 
             # Apply meanshift to get the new location
             ret, track_window = cv2.meanShift(dst, track_window, term_crit)
 
             # Append new window to coordinates
             x,y,w,h = track_window
-
             coords.append((x, y, x+w, y+h))
+
         else:
             break
 
@@ -86,26 +92,69 @@ def track_ball_1(video):
       a list of (min_x, min_y, max_x, max_y) four-tuples containing the pixel
       coordinates of the rectangular bounding box of the ball in each frame.
     """
-    return track_ball(video)
+
+    hough_parameters = {
+        'dp' : 7,
+        'minDist' : 1000,
+        'param1' : 40, 
+        'param2' : 20,
+        'minRadius' : 0,
+        'maxRadius' : 0
+    }
+
+    return track_ball(video, hough_parameters)
+    
         
     
 
 def track_ball_2(video):
     """As track_ball_1, but for ball_2.mov."""
-    return track_ball(video)
+
+    hough_parameters = {
+        'dp' : 7,
+        'minDist' : 1000,
+        'param1' : 40, 
+        'param2' : 20,
+        'minRadius' : 0,
+        'maxRadius' : 0
+    }
+
+    return track_ball(video, hough_parameters)
+
     
 
 
 def track_ball_3(video):
     """As track_ball_1, but for ball_2.mov."""
-    return track_ball(video)
 
+    hough_parameters = {
+        'dp' : 2,
+        'minDist' : 1000,
+        'param1' : 40, 
+        'param2' : 40,
+        'minRadius' : 0,
+        'maxRadius' : 0
+    }
+
+    return track_ball(video, hough_parameters, 0.9)
+    
 
 def track_ball_4(video):
     """As track_ball_1, but for ball_2.mov."""
-    return track_ball(video)
+
+    hough_parameters = {
+        'dp' : 7,
+        'minDist' : 1000,
+        'param1' : 40, 
+        'param2' : 20,
+        'minRadius' : 0,
+        'maxRadius' : 0
+    }
+
+    return track_ball(video, hough_parameters)
 
 def track_face(video):
+    return
     """As track_ball_1, but for face.mov."""
     minX = 0
     cap = video
