8522e3e7de1e5471d74e5aa97fc55529d870cbbb
Nikhil Dixit <njdixit@gmail.com>
2014-11-03 18:01:36 -0600

    ALl tests passing. TODO: extra credit, modify README.

--- project_3/tracking.py
+++ project_3/tracking.py
@@ -10,26 +10,23 @@ import numpy as np
 
 def get_initial_position(frame, hough_parameters):
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
-    #circles = cv2.HoughCircles(frame, cv2.cv.CV_HOUGH_GRADIENT, 7, 1000,       WORKS FOR 1, 2, 4
-     #                           param1=40, param2=20, minRadius=0, maxRadius=0)
-    
-    #circles = cv2.HoughCircles(frame, cv2.cv.CV_HOUGH_GRADIENT, 2, 1000,
-     #                           param1=40, param2=40, minRadius=0, maxRadius=0)
-    
-    circles = cv2.HoughCircles(frame, cv2.cv.CV_HOUGH_GRADIENT, **hough_parameters)
+
+    circles = cv2.HoughCircles(
+        frame,
+        cv2.cv.CV_HOUGH_GRADIENT,
+        **hough_parameters)
 
     circles = np.uint16(np.around(circles))
     return circles[0][0]
 
 
-def track_ball(video, hough_parameters, initial_radius_scalar = 1):
+def track_ball(video, hough_parameters, initial_radius_scalar=1):
 
     # data structure to keep track of all frames
     frame_history = []
 
     # take first frame of the video
-    ret,frame = video.read()
+    ret, frame = video.read()
     height, width, depth = frame.shape
 
     # setup initial pixel history for background subtraction
@@ -43,43 +40,56 @@ def track_ball(video, hough_parameters, initial_radius_scalar = 1):
     coords = []
 
     # setup initial location of window
-    r,h,c,w = int(initial_circle[0] - initial_circle[2]), int(initial_circle[2] * 2 * initial_radius_scalar), initial_circle[1] - initial_circle[2], int(initial_circle[2] * 2 * initial_radius_scalar)
-    track_window = (c,r,w,h)
-    coords.append((initial_circle[0] - initial_circle[2], initial_circle[1] - initial_circle[2], initial_circle[0] + initial_circle[2], initial_circle[1] + initial_circle[2]))
+    r, h, c, w = int(
+        initial_circle[0] - initial_circle[2]), int(
+        initial_circle[2] * 2 * initial_radius_scalar), \
+        initial_circle[1] - initial_circle[2], int(
+            initial_circle[2] * 2 * initial_radius_scalar)
+    track_window = (c, r, w, h)
+    coords.append(
+        (initial_circle[0] -
+         initial_circle[2],
+         initial_circle[1] -
+         initial_circle[2],
+         initial_circle[0] +
+         initial_circle[2],
+         initial_circle[1] +
+         initial_circle[2]))
 
     # set up the ROI for tracking
-    
-    roi = frame[r:r+h, c:c+w]
-    hsv_roi =  cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv_roi, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
-    roi_hist = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180])
-    cv2.normalize(roi_hist,roi_hist,0,255,cv2.NORM_MINMAX)
-    
 
-    # Setup the termination criteria, either 20 iterations or move by atleast 1 pt
+    roi = frame[r:r + h, c:c + w]
+    hsv_roi = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(
+        hsv_roi, np.array(
+            (0., 60., 32.)), np.array(
+            (180., 255., 255.)))
+    roi_hist = cv2.calcHist([hsv_roi], [0], mask, [180], [0, 180])
+    cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX)
+
+    # Setup the termination criteria, either 20 iterations or move by atleast
+    # 1 pt
     term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 30, 1)
 
-    
     while(1):
-        ret ,frame = video.read()
+        ret, frame = video.read()
 
-        if ret == True:
+        if ret:
             hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-            dst = cv2.calcBackProject([hsv],[0],roi_hist,[0,180],1)
-
+            dst = cv2.calcBackProject([hsv], [0], roi_hist, [0, 180], 1)
 
             # Apply meanshift to get the new location
             ret, track_window = cv2.meanShift(dst, track_window, term_crit)
 
             # Append new window to coordinates
-            x,y,w,h = track_window
-            coords.append((x, y, x+w, y+h))
+            x, y, w, h = track_window
+            coords.append((x, y, x + w, y + h))
 
         else:
             break
 
     return coords
- 
+
 
 def track_ball_1(video):
     """Track the ball's center in 'video'.
@@ -94,83 +104,85 @@ def track_ball_1(video):
     """
 
     hough_parameters = {
-        'dp' : 7,
-        'minDist' : 1000,
-        'param1' : 40, 
-        'param2' : 20,
-        'minRadius' : 0,
-        'maxRadius' : 0
+        'dp': 7,
+        'minDist': 1000,
+        'param1': 40,
+        'param2': 20,
+        'minRadius': 0,
+        'maxRadius': 0
     }
 
     return track_ball(video, hough_parameters)
-    
-        
-    
+
 
 def track_ball_2(video):
     """As track_ball_1, but for ball_2.mov."""
 
     hough_parameters = {
-        'dp' : 7,
-        'minDist' : 1000,
-        'param1' : 40, 
-        'param2' : 20,
-        'minRadius' : 0,
-        'maxRadius' : 0
+        'dp': 7,
+        'minDist': 1000,
+        'param1': 40,
+        'param2': 20,
+        'minRadius': 0,
+        'maxRadius': 0
     }
 
     return track_ball(video, hough_parameters)
-
-    
 
 
 def track_ball_3(video):
     """As track_ball_1, but for ball_2.mov."""
 
     hough_parameters = {
-        'dp' : 2,
-        'minDist' : 1000,
-        'param1' : 40, 
-        'param2' : 40,
-        'minRadius' : 0,
-        'maxRadius' : 0
+        'dp': 2,
+        'minDist': 1000,
+        'param1': 40,
+        'param2': 40,
+        'minRadius': 0,
+        'maxRadius': 0
     }
 
     return track_ball(video, hough_parameters, 0.9)
-    
+
 
 def track_ball_4(video):
     """As track_ball_1, but for ball_2.mov."""
 
     hough_parameters = {
-        'dp' : 7,
-        'minDist' : 1000,
-        'param1' : 40, 
-        'param2' : 20,
-        'minRadius' : 0,
-        'maxRadius' : 0
+        'dp': 7,
+        'minDist': 1000,
+        'param1': 40,
+        'param2': 20,
+        'minRadius': 0,
+        'maxRadius': 0
     }
 
     return track_ball(video, hough_parameters)
 
+
 def track_face(video):
-    return
     """As track_ball_1, but for face.mov."""
-    faceCascade = cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
+    faceCascade = cv2.CascadeClassifier(
+        'data/haarcascade_frontalface_default.xml')
     outList = []
     while True:
         # take first frame of the video
-        ret,frame = video.read()
-        if ret == False:
+        ret, frame = video.read()
+        if not ret:
             break
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         # Detect faces in the image
         faces = faceCascade.detectMultiScale(gray,
-                                        scaleFactor=1.1,
-                                        minNeighbors=5,
-                                        minSize=(30, 30),
-                                        flags = cv2.cv.CV_HAAR_SCALE_IMAGE
-                                        )
-        outList.append((faces[0][0], faces[0][1], faces[0][0]+faces[0][2], faces[0][1]+faces[0][3]))
+                                             scaleFactor=1.1,
+                                             minNeighbors=5,
+                                             minSize=(30, 30),
+                                             flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+                                             )
+        outList.append(
+            (faces[0][0],
+             faces[0][1],
+                faces[0][0] +
+                faces[0][2],
+                faces[0][1] +
+                faces[0][3]))
     return outList
-    
